1a1a74d2b8c0704117a141206df091e5eb5384dc
Afifah Nofa Kurnia Rahmadani <afifahrahma711@gmail.com>
2023-05-22 00:14:39 +0700

    afifah_edit_adminlte

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('user.index');
+    }
+    public function ekstrakulikuler()
+    {
+        return view('user.ekstrakulikuler');
+    }
+    public function visimisi()
+    {
+        return view('user.visimisi');
+    }
+    public function karate()
+    {
+        return view('user.ekstra.karate');
+    }
+    public function menari()
+    {
+        return view('user.ekstra.menari');
+    }
+    public function inggris()
+    {
+        return view('user.ekstra.inggris');
+    }
+    public function tik()
+    {
+        return view('user.ekstra.tik');
+    }
+    public function twisada()
+    {
+        return view('user.ekstra.twisada');
+    }
+    public function artikel()
+    {
+        return view('user.artikel');
+    }
+    public function perArtikel()
+    {
+        return view('user.perArtikel');
+    }
+    public function ppdb()
+    {
+        return view('user.ppdb');
+    }
+    public function galeri()
+    {
+        return view('user.galeri');
+    }
+    public function perGaleri()
+    {
+        return view('user.perGaleri');
+    }
+    public function kontak()
+    {
+        return view('user.kontak');
+    }
+    public function dataguru()
+    {
+        return view('user.dataguru');
+    }
+    public function datasiswa()
+    {
+        return view('user.datasiswa');
+    }
+    public function sarana()
+    {
+        return view('user.sarana');
+    }
+    public function prestasi()
+    {
+        return view('user.prestasi');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,72 +15,24 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [IndexController::class, 'index']);
+Route::get('/ekstrakulikuler', [IndexController::class, 'ekstrakulikuler']);
+Route::get('/visimisi', [IndexController::class, 'visimisi']);
+Route::get('/karate', [IndexController::class, 'karate']);
+Route::get('/menari', [IndexController::class, 'menari']);
+Route::get('/inggris', [IndexController::class, 'inggris']);
+Route::get('/tik', [IndexController::class, 'tik']);
+Route::get('/twisada', [IndexController::class, 'twisada']);
+Route::get('/artikel', [IndexController::class, 'artikel']);
+Route::get('/perArtikel', [IndexController::class, 'perArtikel']);
+Route::get('/ppdb', [IndexController::class, 'ppdb']);
+Route::get('/galeri', [IndexController::class, 'galeri']);
+Route::get('/perGaleri', [IndexController::class, 'perGaleri']);
+Route::get('/kontak', [IndexController::class, 'kontak']);
+Route::get('/dataguru', [IndexController::class, 'dataguru']);
+Route::get('/datasiswa', [IndexController::class, 'datasiswa']);
+Route::get('/sarana', [IndexController::class, 'sarana']);
+Route::get('/prestasi', [IndexController::class, 'prestasi']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', function () {
-    return view('user.index');
-});
-
-Route::get('/ekstrakulikuler', function () {
-    return view('user.ekstrakulikuler');
-});
-
-Route::get('/visimisi', function () {
-    return view('user.visimisi');
-});
-
-Route::get('/karate', function () {
-    return view('user.ekstra.karate');
-});
-
-Route::get('/menari', function () {
-    return view('user.ekstra.menari');
-});
-
-Route::get('/inggris', function () {
-    return view('user.ekstra.inggris');
-});
-
-Route::get('/tik', function () {
-    return view('user.ekstra.tik');
-});
-
-Route::get('/twisada', function () {
-    return view('user.ekstra.twisada');
-});
-
-Route::get('/artikel', function () {
-    return view('user.artikel');
-});
-
-Route::get('/ppdb', function () {
-    return view('user.ppdb');
-});
-
-Route::get('/perArtikel', function () {
-    return view('user.perArtikel');
-});
-
-Route::get('/galeri', function () {
-    return view('user.galeri');
-});
-Route::get('/perGaleri', function () {
-    return view('user.perGaleri');
-});
-Route::get('/kontak', function () {
-    return view('user.kontak');
-});
-Route::get('/visimisi', function () {
-    return view('user.visimisi');
-});
-Route::get('/dataguru', function () {
-    return view('user.dataguru');
-});
-Route::get('/datasiswa', function () {
-    return view('user.datasiswa');
-});
-Route::get('/sarana', function () {
-    return view('user.sarana');
-});
-Route::get('/prestasi', function () {
-    return view('user.prestasi');
-});
