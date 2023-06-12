b2f84f86904553016c98d6da0fea875669a9feee
Afifah Nofa Kurnia Rahmadani <afifahrahma711@gmail.com>
2023-06-12 09:06:31 +0700

    kalender

--- app/Http/Controllers/KalenderController.php
+++ app/Http/Controllers/KalenderController.php
@@ -16,7 +16,7 @@ class KalenderController extends Controller
     public function index()
     {
         $kalender = KalenderModel::all();
-        return view('user.datakalender')
+        return view('admin.kalender.kalender')
             ->with('kalender', $kalender);
     }
 
